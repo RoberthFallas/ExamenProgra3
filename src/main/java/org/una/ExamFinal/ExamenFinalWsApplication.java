67c5dca3a12f69e26e2da5f4569c685d8a3f7685
Roberth <roberth123pz@gmail.com>
2020-10-26 23:42:31 -0600

    Todo

---
+++ src/main/java/org/una/ExamFinal/ExamenFinalWsApplication.java
@@ -0,0 +1,13 @@
+package org.una.ExamFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/una/ExamFinal/ExamenFinalWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.ExamFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
